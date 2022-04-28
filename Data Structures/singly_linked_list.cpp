1c40dcc74df9310c7e8e79fc882b66db0216b99f
Syed Abdullah <syed_khoab@outlook.com>
2022-04-28 18:24:19 +0600

    Update singly linkedList

--- Data Structures/singly_linked_list.cpp
+++ Data Structures/singly_linked_list.cpp
@@ -35,6 +35,28 @@ void printList(Node *node)
     node=node->next;
   }
 }
+
+void append(Node **head_ref,int key)
+{
+    Node *new_node=new Node();
+    Node *last=*head_ref;
+
+    new_node->key=key;
+    new_node->next=NULL;
+
+    if(*head_ref==NULL)
+    {
+      *head_ref=new_node;
+      return;
+    }
+    while(last->next!=NULL)
+    {
+      last=last->next;
+    }
+    last->next=new_node;
+    return;
+}
+
 int main()
 {
   Node *head=new Node();
@@ -49,6 +71,7 @@ int main()
   pushFront(&head,20);
   pushBack(&tail,30);
   pushBack(&tail,40);
+  append(&head,70);
 
   printList(head);
 
