db20749bc84f2915a763085f35c27b3c8f075144
Syed Abdullah <syed_khoab@outlook.com>
2022-07-25 18:35:31 +0600

    Update

---
+++ Algorithms/Graph_algorithms/prac2.cpp
@@ -0,0 +1,14 @@
+#include  <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    vector<int> nums={1,2,3};
+    // for(int i:nums)
+    // {
+    //     cout<<i<<" ";
+    // }
+    cout<<nums[1];
+}
--- problems_from_leetcode/prac.cpp
+++ problems_from_leetcode/prac.cpp
@@ -24,14 +24,38 @@ int  Solve(string str,int length)
     return max;
 }
 
+void loop()
+{
+    for(int i=0; i<100000; i++)
+    {
+        for(int j=0; j<1000000;j++)
+        {
+
+        }
+    }
+}
+
 
 int main()
 {
-    int n;
-    for(int i=0;i< 10;i++)
-    {
-        n=rand()%10+20;
-        cout<<n<<endl;
-    }
+    // int n;
+    // for(int i=0;i< 10;i++)
+    // {
+    //     n=rand()%10+20;
+    //     cout<<n<<endl;
+    // }
+
+    time_t  start,end;
+    time(&start);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    loop();
+    time(&end);
+    double  timetaken=double(end-start);
+     cout << "Time taken by program is : " << fixed
+         << timetaken << setprecision(5);
+    cout << " sec " << endl;
 
     }
