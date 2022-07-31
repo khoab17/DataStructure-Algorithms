427f3f6512652e6de74d3eca7016323105b76d45
Syed Abdullah <syed_khoab@outlook.com>
2022-07-31 23:39:07 +0600

    Update

--- test/problem_1.cpp
+++ test/problem_1.cpp
@@ -1,40 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void sovle(string a,string b)
+//Solution 
+void solve(string a, string b)
 {
-    int j=0;
-    int k=-1;
-
-    int b_size=b.length();
-    int a_size=a.length();
-
-    bool match=false;
-
-    for(int i=0; i<a.length(); i++)
+    while(a.length() != b.length() )
     {
-        if(a[i]==b[j])
+        if(b[0]=='1')
         {
-            k=i;
-            break;
+            a[1]=max(a[0],a[1]);
+            a.erase(a.begin());
         }
-    } 
-
-    if(k>=0)
-    {
-        for( int l=k+1; l<= a_size-(b_size-1); l++)
-        {
-            if(a.substr( l , b_size-1)== b.substr(1,b_size-1) )
-            {
-                match=true;
-                cout<<"YES"<<endl;
-                break;
-            }
+        else{
+            a[1]=min(a[0],a[1]);
+            a.erase(a.begin());
         }
     }
-    if(!match)
+
+    if(a==b)
+        cout<<"YES"<<endl;
+    else
         cout<<"NO"<<endl;
 }
+
+// void solve(string a,string b)
+// {
+//     int j=0;
+//     int k=-1;
+
+//     int b_size=b.length();
+//     int a_size=a.length();
+
+//     for(int i=0; i<a.length(); i++)
+//     {
+//         if(a[i]==b[j])
+//         {
+//             k=i;
+//             break;
+//         }
+//     } 
+
+//     if(k>=0)
+//     {
+//         for( int l=k+1; l<= a_size-(b_size-1); l++)
+//         {
+//             if(a.substr( l , b_size-1)== b.substr(1,b_size-1) )
+//             {
+//                 cout<<"YES"<<endl;
+//                 return;
+//             }
+//         }
+//     }
+//     cout<<"NO"<<endl;
+// }
 
 int main()
 {
@@ -44,11 +62,8 @@ int main()
     for(int i=0;  i<t_case; i++)
     {
         int n,m;
-        cin>>n;
-        cin>>m;
         string a,b;
-        cin>>a;
-        cin>>b;
-        sovle(a,b);
+        cin>>m>>n>>a>>b;
+        solve(a,b);
     }
 }
