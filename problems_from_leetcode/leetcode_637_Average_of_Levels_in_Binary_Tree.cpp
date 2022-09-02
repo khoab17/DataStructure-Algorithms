a368d3716a6f3edf125f26bcbb679a79175231a1
Syed Abdullah <syed_khoab@outlook.com>
2022-09-02 14:58:45 +0600

    Update

---
+++ problems_from_leetcode/leetcode_637_Average_of_Levels_in_Binary_Tree.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+
+    unordered_map<int,long long> sum;
+    unordered_map<int ,int> levelNode;
+    int maxlevel=0;
+
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> ans;
+        solve(root,0);
+        for(int i=0; i<=maxlevel; i++){
+            ans.push_back(double(sum[i])/double(levelNode[i]));
+        }
+        return ans;
+    }
+
+   
+    void solve(TreeNode *node , int level){
+        if(!node)
+            return;
+        else{
+            sum[level]+=node->val;
+            ++levelNode[level];
+            maxlevel=max(level,maxlevel);
+            solve(node->left,level+1);
+            solve(node->right,level+1);
+        }
+    }
+    
+};
+
+int main(){
+    TreeNode * root=new TreeNode(3);
+    root->left=new TreeNode(9);
+    root->right=new TreeNode(20);
+    root->right->left=new TreeNode(15);
+    root->right->right=new TreeNode(7);
+    Solution s;
+    vector<double> ans=s.averageOfLevels(root);
+    // for(auto i:ans)
+    // {
+    //     cout<<i<<endl;
+    // }
+    long long a=324e11;
+    cout<<a<<endl;
+}
--- test/ttt.cpp
+++ test/ttt.cpp
@@ -1,11 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
 
-int main()
-{
-    char b='b';
-    const char *a=&b;
-    puts(a);
-    cout<<endl;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        string s,ss;
+        cin>>s>>ss;
+
+        if(s==ss && s[0]==s[1])
+            cout<<"0\n";
+        else{
+            unordered_set<char> seen;
+            int uc=0;
+            for(int i=0; i<2; i++){
+                if(!seen.count(s[i]))
+                {  
+                    uc++;
+                    seen.emplace(s[i]);
+                }
+                if(!seen.count(ss[i]))
+                {
+                    uc++;
+                    seen.emplace(ss[i]);
+                }
+            }   
+            cout<<uc-1<<"\n";
+        }
+    }
+    
 }
+
